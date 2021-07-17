d678caa69214c8cdec94926a886eacafcf3e1f1a
Evan Schoening <evan.schoening@gmail.com>
2021-07-17 10:16:52 -0700

    refactored classes to avoid repeat

--- hr/employees.py
+++ hr/employees.py
@@ -1,74 +1,44 @@
-# This will be the parent class associated with this
-# Also an abstract class, this is not meant to be called
+# needed to nuke and pace
+
+from hr import (
+    SalaryPolicy,
+    CommissionPolicy,
+    HourlyPolicy
+)
+
+from productivity import (
+    ManagerRole,
+    SecretaryRole,
+    SalesRole,
+    FactoryRole
+)
+
 class Employee:
     def __init__(self, id, name):
         self.id = id
         self.name = name
 
-
-class SalaryEmployee(Employee):
+class Manager(Employee, ManagerRole, SalaryPolicy):
     def __init__(self, id, name, weekly_salary):
-        # We want to initialize parent method
-        # We can do this with super
+        SalaryPolicy.__init__(self, weekly_salary)
         super().__init__(id, name)
-        self.weekly_salary = weekly_salary
 
-    def calculate_payroll(self):
-        return self.weekly_salary
-
-class HourlyEmployee(Employee):
-    def __init__(self, id, name, hours_worked, hour_rate):
+class Secretary(Employee, SecretaryRole, SalaryPolicy):
+    def __init__(self, id, name, weekly_salary):
+        SalaryPolicy.__init__(self, weekly_salary)
         super().__init__(id, name)
-        self.hours_worked = hours_worked
-        self.hour_rate = hour_rate
 
-    def calculate_payroll(self):
-        return self.hours_worked * self.hour_rate
-
-
-
-class CommissionEmployee(SalaryEmployee):
+class SalesPerson(Employee, SalesRole, CommissionPolicy):
     def __init__(self, id, name, weekly_salary, commission):
-        # Inherits from the other
-        super().__init__(id, name, weekly_salary)
-        self.commission = commission
+        CommissionPolicy.__init__(self, weekly_salary, commission)
+        super().__init__(id, name)
 
-    def calculate_payroll(self):
-        fixed = super().calculate_payroll()
-        return fixed + self.commission
-
-'''
-The below methods are used to make productivity measurements
-This is also and example of the class explosion principle 
-'''
-class Manager(SalaryEmployee):
-    # Will not use an init method
-    def work(self, hours):
-        print(f'{self.name} yells for {hours} hours.')
-
-class Secretary(SalaryEmployee):
-    # Will not use an init method
-    def work(self, hours):
-        print(f'{self.name} does paper work for {hours} hours.')
-
-
-class SalesPerson(CommissionEmployee):
-    # Will not use an init method
-    def work(self, hours):
-        print(f'{self.name} sells widgets for {hours} hours.')
-
-class FactoryWorker(HourlyEmployee):
-    # Will not use an init method
-    def work(self, hours):
-        print(f'{self.name} constructs widgets for {hours} hours.')
-
-class TemporarySecretary(Secretary, HourlyEmployee):
-    # multiple inheritance example, receive from other classes
-    # being able to add this to certain scenarios
-    # Think method resolution in order when working with Python
-    # Below will bypass the MRO
+class FactoryWorker(Employee, FactoryRole, HourlyPolicy):
     def __init__(self, id, name, hours_worked, hour_rate):
-        HourlyEmployee.__init__(self, id, name, hours_worked, hour_rate)
+        HourlyPolicy.__init__(self, hours_worked, hour_rate)
+        super().__init__(id, name)
 
-    def calculate_payroll(self):
-        return HourlyEmployee.calculate_payroll(self)
+class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
+    def __init__(self, id, name, hours_worked, hour_rate):
+        HourlyPolicy.__init__(self, hours_worked, hour_rate)
+        super().__init__(id, name)
--- hr/productivity.py
+++ hr/productivity.py
@@ -4,5 +4,26 @@ class ProductivitySystem:
         print('Tracking Employee Productivity')
         print('=======================')
         for employee in employees:
-            employee.work(hours)
+            result = employee.work(hours)
+            print(f'{employee.name}: {result}')
         print('')
+
+class ManagerRole:
+    def work(self, hours):
+        return f'screams and yells for {hours} hours.'
+
+class SecretaryRole:
+    # Will not use an init method
+    def work(self, hours):
+        print(f'does paper work for {hours} hours.')
+
+
+class SalesRole:
+    # Will not use an init method
+    def work(self, hours):
+        print(f'sells widgets for {hours} hours.')
+
+class FactoryRole:
+    # Will not use an init method
+    def work(self, hours):
+        print(f'constructs widgets for {hours} hours.')
