2e509717704e9107fa0245ebb800175356d99cc1
Evan Schoening <evan.schoening@gmail.com>
2021-07-25 20:41:25 -0700

    restructuring employees file

--- hr/contacts.py
+++ hr/contacts.py
@@ -2,6 +2,25 @@
 # We can't store it as string in case we want to pull info
 # Between streets, states, cities, etc
 
+# will also create an address mapping class
+# Map ids to address
+
+class AddressBook:
+    def __init__(self):
+        self._employee_address = {
+            1: Address('23 Maple St', 'Kansas City', 'Missouri', '87604'),
+            2: Address('54 Sycamore Lane', 'Huntsville', 'Alabama', '56901'),
+            3: Address('24 Sunny St', 'Baltimore', 'Maryland', '777453'),
+            4: Address('99 Asher Road', 'Miami', 'Florida', '97855'),
+            5: Address('574 Beach blvd', 'Anaheim', 'California', '97680')
+        }
+
+    def get_address(self, employee_id):
+        address = self._employee_address.get(employee_id)
+        if not address:
+            raise ValueError('Invalid Address id')
+        return address
+
 class Address:
     def __init__(self, street, city, state, zipcode, street2=''):
         self.street = street
--- hr/employees.py
+++ hr/employees.py
@@ -1,45 +1,72 @@
 # needed to nuke and pace
 
-from hr import (
-    SalaryPolicy,
-    CommissionPolicy,
-    HourlyPolicy
-)
+from contacts import AddressBook
+from hr import PayrollSystem
+from productivity import ProductivitySystem
 
-from productivity import (
-    ManagerRole,
-    SecretaryRole,
-    SalesRole,
-    FactoryRole
-)
 
+
+# Act as a database, not an actual database
+# List of dictionarys,
+class EmployeeDatabase:
+    def __init__(self):
+        self._employees = [
+            {
+            'id': 1,
+            'name': 'John Doe',
+            'role': 'manager'
+            },
+
+            {
+                'id': 2,
+                'name': 'Jane Doe',
+                'role': 'secretary'
+            }
+
+
+            {
+                'id': 3,
+                'name': 'John Cena',
+                'role': 'factory'
+            }
+
+
+            {
+                'id': 4,
+                'name': 'Bruce Willis',
+                'role': 'sales'
+            }
+
+
+            {
+                'id': 5,
+                'name': 'Satoshi',
+                'role': 'manager'
+            }
+
+        ]
+
+        self.productivity = ProductivitySystem()
+        self.address = AddressBook()
+        self.payroll = PayrollSystem()
+
+    def employees(self):
+        # This returns a new employee data set in one liner
+        # Need to write this method for usage
+        return[self._create_employee(**data) for data in self._employees]
+
+    # _ means method should only be called in this class
+    def _create_employee(self, id, name, role):
+
+
+
+
+# Going to refactor this code
+# using the dict ID mappings as a psuedo dastabase
 class Employee:
     def __init__(self, id, name):
         self.id = id
         self.name = name
         self.address = None
 
-class Manager(Employee, ManagerRole, SalaryPolicy):
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        super().__init__(id, name)
 
-class Secretary(Employee, SecretaryRole, SalaryPolicy):
-    def __init__(self, id, name, weekly_salary):
-        SalaryPolicy.__init__(self, weekly_salary)
-        super().__init__(id, name)
-
-class SalesPerson(Employee, SalesRole, CommissionPolicy):
-    def __init__(self, id, name, weekly_salary, commission):
-        CommissionPolicy.__init__(self, weekly_salary, commission)
-        super().__init__(id, name)
-
-class FactoryWorker(Employee, FactoryRole, HourlyPolicy):
-    def __init__(self, id, name, hours_worked, hour_rate):
-        HourlyPolicy.__init__(self, hours_worked, hour_rate)
-        super().__init__(id, name)
-
-class TemporarySecretary(Employee, SecretaryRole, HourlyPolicy):
-    def __init__(self, id, name, hours_worked, hour_rate):
-        HourlyPolicy.__init__(self, hours_worked, hour_rate)
-        super().__init__(id, name)
